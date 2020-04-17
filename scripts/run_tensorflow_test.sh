0cc498bc0b45988bd5f5d7d1e43fc72557a7ce77
Darrell Schrag <drschrag@DRS-MacBook-Pro.local>
2020-04-16 19:59:26 -0500

    try it again

--- scripts/run_tensorflow_test.sh
+++ scripts/run_tensorflow_test.sh
@@ -13,8 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+echo "Setting up Tensorflow docker image"
+BASEDIR="$(dirname "$0")"
+
 # build a PowerAI docker image (Docker file should already be there)
-docker build . -t nvidia-powerai -f /tmp/scripts/Dockerfile.txt
+docker build . -t nvidia-powerai -f ${BASEDIR}/Dockerfile.txt
 
 # run a few tensorflow tests
 nvidia-docker run --rm -v ${BASEDIR}/results:/tmp/results  nvidia-powerai bash -c "source /opt/DL/tensorflow/bin/tensorflow-activate; python /opt/DL/tensorflow/lib/python2.7/site-packages/tensorflow/examples/learn/multiple_gpu.py --test_with_fake_data > /tmp/results/results.txt 2>&1"
@@ -26,8 +29,9 @@ now=`date +"%m_%d_%Y-%H:%M:%S"`
 wget https://dl.min.io/client/mc/release/linux-ppc64le/mc
 chmod +x mc
 
+echo "Move Tensorflow results to ICOS"
 # move the results to an object storage bucket
 ./mc config host add icos $1 $2 $3
-./mc cp /tmp/scripts/results/results.txt icos/$4/results-$now.txt
+./mc cp ${BASEDIR}/results/results.txt icos/$4/results-$now.txt
 rm mc
 echo "SUCCESS: Tensorflow setup, sample test run, and output uploaded to ICOS bucket"
